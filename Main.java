6bfb9040aff9f745f0985994b6bcfce3d3a387bc
AngryAndry <67871858+AngryAndry@users.noreply.github.com>
2020-10-06 00:21:34 +0200

    First commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        String x = scanner.next();
+        String b = scanner.next();
+
+
+
+        String [] arab = new String[]{"10","1","2","3","4","5","6","7","8","9"};
+        String [] rome = new String[]{"X","I","II","III","IV","V","VI","VII","VIII","IX"};
+        for (int c =0;c<arab.length;c++){
+            if (rome[c].equals(a)){
+                a = arab[c];
+            }
+            if (rome[c].equals(b)){
+                b = arab[c];
+            }
+
+        }
+        int a1 = Integer.parseInt(a.trim());
+        int b1 = Integer.parseInt(b.trim());
+
+
+        if (x.equals("+")) {
+            System.out.println(a1 + b1);
+        }
+
+    }
+}
---
+++ Roma.java
@@ -0,0 +1,12 @@
+public enum Roma {
+    I(1), II(2), III(3), IV(4), V(5), VI(6);
+    private int Arb;
+
+    Roma(int Arb) {
+        this.Arb = Arb;
+
+    }
+    public int getArb(){
+        return Arb;
+    }
+}
